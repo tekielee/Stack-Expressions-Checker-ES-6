97f8f379cad243fa69589625ea017679ff96921a
Cuong Le <tekielee@yahoo.com>
2021-04-08 16:17:35 -0700

    initial commit

--- expressions_checker.js
+++ expressions_checker.js
@@ -4,31 +4,87 @@ class Stack {
 		this.stack = [];
 	}
 	
+	isEmpty() {
+		if(this.stack === 'undefined' || this.stack === null || this.stack === 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	isFull() {
+		if(this.limit <= this.stack.length) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	push(item) {
+		if(this.isFull()) {
+			console.log('Stack is full');
+		} else {
+			this.stack.push(item);
+		}
+	}
+	
 	pop() {
-		if(this.stack === undefined || this.stack === null || this.stack.length === 0) {
+		if(this.isEmpty()) {
 			console.log('Stack is empty');
 		} else {
 			return this.stack.pop();
 		}
 	}
 	
-	push(newItem) {
-		if(this.stack.length < this.limit) {
-			this.stack.push(newItem);
-		} else {
-			console.log('Stack is full');
-		}	
-	}
-	
 	top() {
-		return this.stack[this.stack.length - 1];
+		if(this.isEmpty()) {
+			console.log('Stack is empty');
+		} else {
+			return this.stack[this.stack.length - 1];
+		}
 	}
 	
-	isEmpty() {
-		if(this.stack === undefined || this.stack === null || this.stack.length === 0) {
-			return true;
-		} else {
-			return false;
-		}
+	expressionChecker(str) {
+		let valid = true;
+		str = Array.from(str.split(' ').join(''));
+		for(let i = 0; i < str.length; i++) {
+			let char = str[i];
+			switch(char) {
+				case '(':
+				case '{':
+				case '[':
+					this.push(str[i]);
+					break;
+				case ')':
+				case '}':
+				case ']':
+				if(this.isEmpty()) {
+					valid = false;
+				} else {
+					let last = this.stack.pop();
+					if((char == ')' && last != '(') || (char == '}' && last != '{') || (char == ']' && last != '[')) {
+						valid = false;
+					} 
+				}
+				break;
+			}
+			if(!valid) {
+				break;
+			}
+		}	
+	
+		return valid;
+	}
+}
+
+const stack = new Stack();
+
+let expressions = ['8 * (9 - 2) + { ( 4 * 5) / ( 2 * 2) }', '5 * 8 * 9 / ( 3 * 2 ) )', '[{ (2 * 7) + ( 15 - 3) ]'];
+for(let i = 0; i < expressions.length; i++) {
+	let valid = stack.expressionChecker(expressions[i]);
+	if(valid) {
+		console.log('Expression is valid');
+	} else {
+		console.log('Expression is not valid');
 	}
 }
